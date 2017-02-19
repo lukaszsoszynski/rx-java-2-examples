e7c9397ea7371ef4a646662f4b794f0640e2a418
Lukasz Soszynski <lsoszyn@gmail.com>
2017-02-19 20:04:41 +0100

    Getting data from database using RX java

---
+++ src/main/java/com/impaqgroup/training/reactive/RxApplication.java
@@ -0,0 +1,12 @@
+package com.impaqgroup.training.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxApplication.class, args);
+	}
+}
---
+++ src/main/java/com/impaqgroup/training/reactive/ex00jdbc/reader/sync/BillingService.java
@@ -0,0 +1,29 @@
+package com.impaqgroup.training.reactive.ex00jdbc.reader.sync;
+
+import static com.impaqgroup.training.reactive.ex00jdbc.reader.JdbcReaderApplication.SQL_QUERY;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.impaqgroup.training.reactive.ex00jdbc.BillingRecord;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class BillingService {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Transactional
+    public List<BillingRecord> findBillingRecords() {
+        log.info("Loading all billing data");
+        return jdbcTemplate.query(SQL_QUERY, new BeanPropertyRowMapper<>(BillingRecord.class));
+    }
+}
