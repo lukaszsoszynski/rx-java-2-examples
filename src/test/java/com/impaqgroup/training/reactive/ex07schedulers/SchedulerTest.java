3d35fdbe17bd258ede835c76d77b8c01f0ab9446
Lukasz Soszynski <lsoszyn@gmail.com>
2017-05-04 20:29:30 +0200

    Schedulers and socket example

---
+++ src/test/java/com/impaqgroup/training/reactive/ex07schedulers/SchedulerTest.java
@@ -0,0 +1,288 @@
+package com.impaqgroup.training.reactive.ex07schedulers;
+
+import java.util.concurrent.*;
+
+import org.junit.Test;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.observables.ConnectableObservable;
+import io.reactivex.schedulers.Schedulers;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SchedulerTest {
+
+    @Test
+    public void withoutSchedulers(){
+        Observable<Long> observable = createSlowObservable();
+
+        Observable<String> slowObservableWithOperations = observable
+                .map(this::slowIdentity)
+                .map(this::slowToString);
+
+        //this is blocking, why?
+        slowObservableWithOperations.subscribe(value -> log.info("Value from slow observable {}", value));
+    }
+
+    private Observable<Long> createSlowObservable() {
+        return Observable.create(emitter -> {
+               for(long i = 0; i < 3; ++i){
+                   sleep(1000);
+                   log.info("Next value {} will be emitted.", i);
+                   emitter.onNext(i);
+               }
+                sleep(1000);
+                emitter.onComplete();
+                log.info("Slow observable completed");
+            });
+    }
+
+    private Long slowIdentity(Long value) {
+        sleep(1000);
+        log.info("Slow identity transformation executed for value {}", value);
+        return value;
+    }
+
+    private String slowToString(Long integer){
+        sleep(1000);
+        log.info("Slow to string executed for value {}", integer);
+        return integer.toString();
+    }
+
+    @SneakyThrows
+    private void sleep(long timeout){
+        TimeUnit.MILLISECONDS.sleep(timeout);
+    }
+
+    @Test
+    public void withoutSchedulersTwoSubscribers(){
+        Observable<Long> observable = createSlowObservable();
+
+        Observable<String> slowObservableWithOperations = observable
+                .map(this::slowIdentity)
+                .map(this::slowToString);
+
+        //what is expected test execution time?
+        slowObservableWithOperations.subscribe(value -> log.info("Value from subscriber ONE {}", value));
+        slowObservableWithOperations.subscribe(value -> log.info("Value from subscriber TWO {}", value));
+    }
+
+    @Test
+    public void connectableObservable(){
+        Observable<Long> observable = createSlowObservable();
+
+        ConnectableObservable<String> connectableObservable = observable
+                .map(this::slowIdentity)//<-- how many times slowIdentity will be executed?
+                .map(this::slowToString)//<-- how many times slowToString will be executed?
+                .publish();// <-- here connectable observable is created
+
+        log.info("Add subscribers");
+        connectableObservable.subscribe(value -> log.info("Value from subscriber ONE {}", value));
+        connectableObservable.subscribe(value -> log.info("Value from subscriber TWO {}", value));
+
+        log.info("Two subscribers added, now let's connect");
+        connectableObservable.connect();
+    }
+
+    @Test
+    public void hotConnectableObservable(){
+        Observable<Long> observable = createSlowObservable();
+
+        ConnectableObservable<String> connectableObservable = observable
+                .map(this::slowIdentity)//<-- how many times slowIdentity will be executed?
+                .map(this::slowToString)//<-- how many times slowToString will be executed?
+                .publish();// <-- here connectable observable is created
+
+
+        log.info("Connect called before two subscriber added.");
+        connectableObservable.connect();
+
+        log.info("Two subscriber will be added");
+        //How many events subscriber receive
+        connectableObservable.subscribe(value -> log.info("Value from subscriber ONE {}", value));
+        connectableObservable.subscribe(value -> log.info("Value from subscriber TWO {}", value));
+    }
+
+    @Test
+    public void shouldSubscribeOnScheduler(){
+        Observable<Long> observable = createSlowObservable();
+
+        Observable<String> observableWithTransformations = observable
+                .map(this::slowIdentity)
+                .map(this::slowToString)
+                .subscribeOn(Schedulers.io());//<-- let's run in in separate thread
+
+        //what does it print?
+        observableWithTransformations.subscribe(value -> log.info("Get value {}", value));
+    }
+
+    @Test
+    public void shouldSubscribeOnScheduler2(){
+        Observable<Long> observable = createSlowObservable();
+
+        Observable<String> observableWithTransformations = observable
+                .map(this::slowIdentity)
+                .map(this::slowToString)
+                .subscribeOn(Schedulers.io());//<-- let's run in in separate thread
+
+        //what does it print?
+        observableWithTransformations.subscribe(value -> log.info("Get value {}", value));
+        sleep(10000);
+    }
+
+    @Test
+    public void twoConcurrentSubscriptions(){
+        Observable<Long> observable = createSlowObservable();
+
+        Observable<String> observableWithTransformations = observable
+                .map(this::slowIdentity)
+                .map(this::slowToString)
+                .subscribeOn(Schedulers.io());//<-- let's run in in separate thread
+
+        //how many thread can you see
+        observableWithTransformations.subscribe(value -> log.info("Subscriber ONE {}", value));
+        observableWithTransformations.subscribe(value -> log.info("Subscriber TWO {}", value));
+        sleep(10000);
+    }
+
+    @Test
+    public void manyInvocationOfSubscribeOnIsPointless(){
+        Observable<Long> observable = createSlowObservable();
+
+        Observable<String> observableWithTransformations = observable
+                .subscribeOn(Schedulers.computation())//<-- Run it on computation scheduler
+                .map(this::slowIdentity)
+                .subscribeOn(Schedulers.newThread())//<-- Run it on New Thread scheduler
+                .map(this::slowToString)
+                .subscribeOn(Schedulers.io());//<-- Run it on IO scheduler
+
+        //which schedulers are used?
+        observableWithTransformations.subscribe(value -> log.info("Got value {}", value));
+        sleep(10000);
+    }
+
+    @Test
+    public void shouldObserveOnOtherScheduler(){
+        Observable<Long> observable = createSlowObservable();
+
+        Observable<String> observableWithTransformations = observable
+                .observeOn(Schedulers.computation())//<-- Observe on computation scheduler
+                .map(this::slowIdentity)
+                .observeOn(Schedulers.newThread())//<-- Observe on New Thread scheduler
+                .map(this::slowToString)
+                .subscribeOn(Schedulers.io());
+
+        //which schedulers are used?
+        observableWithTransformations.subscribe(value -> log.info("Got value {}", value));
+        sleep(10000);
+    }
+
+    @Test
+    public void shouldExecuteBlockingGet(){
+        Observable<Long> observable = createSlowObservable();
+
+        Observable<String> observableWithTransformations = observable
+                .map(this::slowIdentity)
+                .map(this::slowToString)
+                .subscribeOn(Schedulers.io());//<-- run it on IO scheduler
+
+        //what does it print, in which thread?
+        observableWithTransformations.blockingForEach(value -> log.info("Get value in blocking manner."));
+        //there is plenty of blocking... method
+    }
+
+    @Test
+    public void manyMethodAcceptScheduler(){
+        Observable.interval(100, TimeUnit.MILLISECONDS, Schedulers.io()) //<-- use provided scheduler
+                .timeout(1, TimeUnit.SECONDS, Schedulers.computation())//<-- use provided scheduler
+                .take(3)
+                .subscribe(value -> log.info("Got value {}", value));
+        sleep(2000);
+    }
+
+    @Test
+    public void eventsEmittedTooQuickly(){
+        Observable.interval(1, TimeUnit.MILLISECONDS, Schedulers.computation())//<-- emits 1000 event per second
+                .take(5)
+                .map(this::slowIdentity)//<-- takes one second
+                .map(this::slowToString)//<-- takes one second
+                .subscribe(value -> log.info("How many message per sec will be printed?"));
+        sleep(10100);
+    }
+
+    @Test
+    public void eventsEmittedTooQuickly2(){
+        Observable.interval(1, TimeUnit.MILLISECONDS, Schedulers.computation())//<-- emits 1000 event per second
+                .take(5)
+                .observeOn(Schedulers.io()) //<-- scheduler added
+                .map(this::slowIdentity)
+                .map(this::slowToString)
+                .subscribe(value -> log.info("How many message per sec will be printed? Is it better?"));
+        sleep(10100);
+    }
+
+    @Test
+    public void eventsEmittedTooQuickly3FlatMapAndSubscribeOn() {
+        Observable.interval(1, TimeUnit.MILLISECONDS, Schedulers.computation())//<-- emits 1000 event per second
+                .take(5)
+                .flatMap(value -> Observable.just(value).map(this::slowIdentity).subscribeOn(Schedulers.io()))//<-- flatMap + subscribeOn
+                .flatMap(value -> Observable.just(value).map(this::slowToString).subscribeOn(Schedulers.io()))//<-- flatMap + subscribeOn
+                .subscribe(value -> log.info("How many message per sec will be printed? What about order? {}", value));
+        sleep(3000);
+    }
+
+
+    @Test
+    public void ownBadScheduler(){
+        Scheduler scheduler = Schedulers.from(Executors.newCachedThreadPool());//<-- own scheduler
+
+        Observable<Long> slowObservable = createSlowObservable();
+
+        slowObservable
+                .subscribeOn(scheduler)
+                .subscribe(value -> log.info("Get value {}", value));
+        sleep(3100);
+    }
+
+    @Test
+    public void ownGoodScheduler(){
+         /*@formatter:off*/
+        ThreadFactory threadFactory = new ThreadFactoryBuilder()
+                .setDaemon(false)
+                .setNameFormat("custom-descriptive-thread-name" + "-%d")//<-- very important, present in logs and JMX
+                .setUncaughtExceptionHandler((t, e) -> log.error("Uncaught exception in RxJava scheduler thread {}", t.getName(), e))//<-- all exceptions must be logged.
+                .build();
+        ExecutorService executorService = new ThreadPoolExecutor(3,//<-- three thread almost always available
+                15,//<-- do not create more then 15 threads
+                30, TimeUnit.SECONDS,//<-- release thread after 30s if there is not task for it
+                new LinkedBlockingQueue<>(15),//<-- if no thread available submit no more then 15 task in non blocking fashion
+                new ThreadFactoryLoggableWrapper(threadFactory));//<-- log thread creation
+        /*@formatter:on*/
+        Scheduler goodScheduler = Schedulers.from(executorService);//here is good scheduler<--
+
+        Observable<Long> slowObservable = createSlowObservable();
+
+        slowObservable
+                .subscribeOn(goodScheduler)
+                .subscribe(value -> log.info("Get value {}", value));
+        sleep(3100);
+    }
+
+    @RequiredArgsConstructor
+    private static class ThreadFactoryLoggableWrapper implements ThreadFactory{
+
+        private final ThreadFactory threadFactory;
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread thread = threadFactory.newThread(r);
+            log.debug("New thread '{}' created for RxJava scheduler", thread.getName());
+            return thread;
+        }
+    }
+}
---
+++ src/test/java/com/impaqgroup/training/reactive/in00socket/SocketTest.java
@@ -0,0 +1,118 @@
+package com.impaqgroup.training.reactive.in00socket;
+
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.*;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.junit.Test;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SocketTest {
+
+    @Test
+    public void singleThreadServer() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(12081);
+        while (true) {
+            try (Socket clientSocket = serverSocket.accept()) {//<-- wait and return client socket when client is connected
+                blockingClientHandler(clientSocket);
+            }
+        }
+    }
+
+    @SneakyThrows
+    private void blockingClientHandler(Socket clientSocket) {
+        log.info("New client connected.");
+        try {
+            try (PrintWriter writer = new PrintWriter(clientSocket.getOutputStream())) {
+                log.info("Sending data to network client.");
+                writer.println("What is your name?");
+                writer.flush();
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    log.info("Waiting for data from network client.");
+                    String name = reader.readLine();//<-- blocking waiting for data from client
+                    writer.println(String.format("%s is very nice name.", name));
+                    writer.flush();
+                }
+            }
+        }finally {
+            log.info("Client disconnected.");
+            clientSocket.close();
+        }
+    }
+
+    @Test
+    public void newThreadPerClientServer() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(12081);
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            Thread thread = new Thread(() -> blockingClientHandler(clientSocket));
+            thread.start();
+        }
+    }
+
+    @Test
+    public void threadPoolServer() throws IOException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+        ServerSocket serverSocket = new ServerSocket(12081);
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            threadPool.submit(() -> blockingClientHandler(clientSocket));
+        }
+    }
+
+    @Test
+    public void nonBlockingSingleServer() throws IOException {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.bind(new InetSocketAddress("localhost", 12081));
+        serverSocketChannel.configureBlocking(false);
+        Selector selector = Selector.open();
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        while (true) {
+            log.info("Check selector");
+            if(selector.select() == 0){
+                continue;
+            }
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+            while (iterator.hasNext()){
+                SelectionKey selectionKey = iterator.next();
+                iterator.remove();
+                log.info("Got new selection key");
+                if(selectionKey.isAcceptable()){
+                    log.info("is acceptable");
+                    ServerSocketChannel serverChanel = (ServerSocketChannel) selectionKey.channel();
+                    SocketChannel clientChanel = serverChanel.accept();
+                    if(clientChanel != null) {
+                        log.info("has client channel");
+                        clientChanel.configureBlocking(false);
+                        clientChanel.register(selector, SelectionKey.OP_READ);
+                        clientChanel.write(Charset.defaultCharset().encode(CharBuffer.wrap("What is your name?\n")));
+                    }
+                }
+                if(selectionKey.isReadable()){
+                    log.info("Is readable");
+                    SocketChannel channel = (SocketChannel) selectionKey.channel();
+                    channel.configureBlocking(false);
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+                    channel.read(byteBuffer);
+                    byteBuffer.flip();
+                    String name = new String(byteBuffer.array()).trim();
+                    byteBuffer.clear();
+                    log.info("String read {}", name);
+                    channel.write(Charset.defaultCharset().encode(String.format("%s is very nice name\n", name)));
+                    channel.close();
+                }
+            }
+        }
+    }
+}
